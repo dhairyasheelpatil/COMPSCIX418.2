b264381862ba6e1645a4ab8d078ef4c9b5c5a910
Sahar Mostafa <smostafa@Sahars-MacBook-Air.local>
2020-08-18 11:04:10 -0700

    adding solution to refactor

--- session 1/refactor.java
+++ session 1/refactor.java
@@ -1,5 +1,46 @@
 public class Refactor {
 
+    private static class Params {
+        final int width;
+        final int height;
+        public Params(int width, int height){
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    private enum ShapeCondition {
+        RECTANGLE(
+                params -> {
+                    return height != width;
+                }
+        ),
+        SQUARE(
+                params -> {
+                    return height != width;
+                }
+        );
+
+        final Predicate<Params> matchesCondition;
+
+        ShapeCondition(final Predicate<Params> matchesCondition){
+            this.matchesCondition = matchesCondition;
+        }
+
+        private static ShapeCondition mapFromParams(final Params params) {
+            return Arrays.stream(values()).filter(type -> type.matchesCondition.test(params)).findFirst().orElse(DEFAULT);
+        }
+    }
+
+    public boolean apply(Shape shape, ShapeCondition shapeCondition) {
+        return shapeCondition == ShapeCondition.mapFromParams(new Params(shape.getWidth(), shape.getHeight()));
+    }
+
     public static void main(String[] args) {
+        Refactor refactor = new Refactor();
+        Shape shape = new Square();
+        if (refactor.apply(shape, ShapeCondition.SQUARE)) {
+            // apply any action needs to be taken around this shape classification
+        }
     }
 }
