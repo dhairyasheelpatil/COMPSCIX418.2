9e8e8063ffab9d4b5d1c022eba1489ad28b582ff
qiuhan-sun <qiuhansun96@gmail.com>
2020-08-24 22:40:32 -0500

    commit first try

--- session 2/refactor/main.java
+++ session 2/refactor/main.java
@@ -1,78 +1,106 @@
+package refactor;
+
+import java.util.List;
+
+// Polynorphism should be used here. UserRegistration should be an abstract class
 
 public class UserRegistration {
-  public List<PCRegistration> _pcRegistrations;
-  public List<MacBookRegistration> _macBookRegistrations;
-  public String _userName;
+    public List<PCRegistration> _pcRegistrations;
+    public List<MacBookRegistration> _macBookRegistrations;
+    public String _userName;
 
-public UserRegistration (String userName)	{
-		_userName = userName;
-	};
+    public UserRegistration(String userName) {
+        _userName = userName;
+    }
 
-	public UserRegistration ()	{};
+    ;
 
-	public void purchase(PCRegistration pcRegistration)	{
-		_pcRegistrations.add(pcRegistration);
-	};
+    public UserRegistration() {
+    }
 
-	public void purchase(MacBookRegistration macRegistration)	{
-		_macBookRegistrations.add(macRegistration);
-	};
+    ;
 
-	public String receipt() {
-		String receipt;
-		for (pcRegistration in _pcRegistrations) {
-			receipt += String.valueOf(pcRegistration.getPrice()) + ","
-		}
-		for (macBookRegistration price in _macBookRegistrations) {
-			receipt += String.valueOf(macBookRegistration.getPrice()) + ","
-		}
-		return receipt.substring(0, receipt.length() - 1);
-	};
+    // Repeated method for purchase
+    public void purchase(PCRegistration pcRegistration) {
+        _pcRegistrations.add(pcRegistration);
+    }
+
+    ;
+
+    public void purchase(MacBookRegistration macRegistration) {
+        _macBookRegistrations.add(macRegistration);
+    }
+
+    ;
+
+    public String receipt() {
+        String receipt;
+        // Repeated code for getting the receipt
+        for (pcRegistration in _pcRegistrations) {
+            receipt += String.valueOf(pcRegistration.getPrice()) + ","
+        }
+        for (macBookRegistration price in _macBookRegistrations){
+            receipt += String.valueOf(macBookRegistration.getPrice()) + ","
+        }
+        return receipt.substring(0, receipt.length() - 1);
+    }
+
+    ;
 }
 
+// PCRegistration should extend UserRegistration
 public class PCRegistration {
-  public String _name;
-  public double _price;
+    public String _name;
+    public double _price;
 
-  public PCRegistration (String name) {
-		_name = name;
-	};
+    public PCRegistration(String name) {
+        _name = name;
+    }
 
-	public PCRegistration () {};
+    ;
 
-	public String name() {
-		return _name;
-	}
+    public PCRegistration() {
+    }
 
-	public String toString() {
-		return _name;
-	}
+    ;
 
-	public double getPrice() {
-		return _price;
-	}
+    public String name() {
+        return _name;
+    }
+
+    public String toString() {
+        return _name;
+    }
+
+    // getPrice should be in the super class
+    public double getPrice() {
+        return _price;
+    }
 }
 
-
+// MacBookRegistration should extend UserRegistration
 public class MacBookRegistration {
-  public String _name;
-  public double _price;
+    public String _name;
+    public double _price;
 
-  public MacBookRegistration(String name) {
-		_name = name;
-	}
+    public MacBookRegistration(String name) {
+        _name = name;
+    }
 
-	public MacBookRegistration() {};
+    public MacBookRegistration() {
+    }
 
-	public String name()	{
-		return _name;
-	}
+    ;
 
-	public String toString() {
-		return _name;
-	}
+    public String name() {
+        return _name;
+    }
 
-	public double getPrice() {
-		return _price;
-	}
+    public String toString() {
+        return _name;
+    }
+
+    public double getPrice() {
+        return _price;
+    }
 }
